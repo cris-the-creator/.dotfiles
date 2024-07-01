40f4ee26f14e3b69e105ea9b2d4bbc00f0974717
cris-the-creator <contact@corneascorner.dev>
2024-07-01 09:42:07 +0200

    multicursor

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -284,5 +284,22 @@ require('lazy').setup({
     keys = {
       { "<leader>yy", "<cmd>Yazi<CR>", desc = "Toggle Yazi" },
     },
+  },
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+        {
+            mode = { 'v', 'n' },
+            '<Leader>mm',
+            '<cmd>MCstart<cr>',
+            desc = 'Create a selection for selected text or word under the cursor',
+        },
+    },
   }
 })
